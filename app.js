ab080445c6ca2f28d84aa9f3ab1e2c4ae0c8b935
hz3r0g <hzerog161@gmail.com>
2026-03-27 14:29:06 +0700

    Prepare for Render: use env PORT and add .env.example

--- app.js
+++ app.js
@@ -80,7 +80,7 @@ app.use((req, res) => {
 
 websocketController.initWebSocketHandlers(wss);
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 server.listen(PORT, '0.0.0.0', () => {
-  console.log(`Server is running on http://0.0.0.0:${PORT}`);
+    console.log(`Server is running on http://0.0.0.0:${PORT}`);
 });
